6595427347ec80c3b7289a84aca7edf0a405316d
Lakshaysehagl01 <sehgall333@gmail.com>
2025-03-24 00:55:44 +0530

    Adding openzepplin contracts

--- src/AutoSell.sol
+++ src/AutoSell.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.0;
 
 contract AutoSellEth{
-    import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+    import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 }
